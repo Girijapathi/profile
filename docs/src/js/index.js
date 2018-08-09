7e7e389180d3efeab0d637261e9202af13b12b0f
GM042816 <girijapathi.math@cerner.com>
2018-08-09 13:28:27 +0530

    Update: Added events

---
+++ docs/src/js/index.js
@@ -0,0 +1,29 @@
+function loadHome(){
+    const test =  document.getElementById("article-body");
+    test.innerHTML= `<p>Hey, thanks for showing interest to read about me &hearts;</p>
+    <p>I am from small village of Bidar district in Karnataka, India. I can speak, read and write Kannada, Hindi and English languages fluently. I love to play badminton and watch movies in freetime.</p>`;
+}
+
+function loadAboutMe(){
+    const test =  document.getElementById("article-body");
+    test.innerHTML= `<p>Associate software engineer, working with Cerner Health care, India.
+    </p>
+    <ul>
+    Hobbies:
+    <li>Learnign new technologies</li>
+    <li>Rreacding tech blogs</li>
+    <li>Fascinated about mobile operating systems</li>
+    <li>Love Music</li>
+    <li>Playing badminton</li>
+    </ul>`;
+}
+
+function loadContact(){
+    const test =  document.getElementById("article-body");
+    test.innerHTML =`<p>Girijapathi D Math</p>
+    <p>#1-222</p>
+    <p>Bangalore</p>
+    <p>India</p>
+    <p>560045</p>`;
+    
+}
