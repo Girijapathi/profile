94d4e162422274cd57deee6f714365b9388e6be9
GM042816 <girijapathi.math@cerner.com>
2018-08-09 14:19:44 +0530

    Fix: Missed to update text in event handlers

--- docs/src/js/index.js
+++ docs/src/js/index.js
@@ -1,7 +1,8 @@
 function loadHome(){
     const test =  document.getElementById("article-body");
     test.innerHTML= `<p>Hey, thanks for showing interest to read about me &hearts;</p>
-    <p>I am from small village of Bidar district in Karnataka, India. I can speak, read and write Kannada, Hindi and English languages fluently. I love to play badminton and watch movies in freetime.</p>`;
+    <p>I am from small village of Bidar district in Karnataka, India. I can speak, read and write Kannada, Hindi and English languages fluently. I love to play badminton and watch movies in freetime.</p>
+    <p>I like to create website and webapplications, using latest Technologies like HTML5, CSS3, ReactJs.</p>`;
 }
 
 function loadAboutMe(){
